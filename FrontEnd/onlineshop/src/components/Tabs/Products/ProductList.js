3ae11851c14530cbd3afbaf883a41dd6884f7e08
Emil <emilbucoveanu@yahoo.com>
2023-03-08 19:46:46 +0200

    Sex products update

--- FrontEnd/onlineshop/src/components/Tabs/Products/ProductList.js
+++ FrontEnd/onlineshop/src/components/Tabs/Products/ProductList.js
@@ -47,33 +47,33 @@ function ProductList({ sex, filters, refreshed, isAdmin }) {
 
         case "F":
 
-            products = [{ name: "Tricou gucci barbati", marca: "Gucci", category: "tricou", sex: "M", price: "300", disc: false },
-            { name: "Hanorac Chanel barbati", sex: "M", marca: "Chanel", category: "hanorac", price: "300", disc: false },
-            { name: "Geaca Burberry barbati", sex: "M", marca: "Burberry", category: "geaca", price: "300", disc: false },
-            { name: "Sosete Zara barbati", sex: "M", marca: "Burberry", category: "sosete", price: "300", category: "", disc: false },
-            { name: "Pulover H&M barbati", sex: "M", marca: "", category: "pulover", price: "300", disc: false },
-            { name: "Tricou Pull&Bear barbati", sex: "M", marca: "Burberry", category: "tricou", price: "300", disc: false },
-            { name: "Camasa gucci barbati", sex: "M", marca: "Burberry", category: "camasa", price: "200", disc: true },
-            { name: "Caciula Chanel barbati", sex: "M", marca: "Burberry", category: "caciula", price: "300", disc: false },
-            { name: "Manusi Burberry barbati", sex: "M", marca: "Burberry", category: "manusi", price: "300", disc: false },
-            { name: "Adiasi Zara barbati", sex: "M", marca: "Burberry", category: "incaltari", price: "300", disc: false },
-            { name: "Pantaloni H&M barbati", sex: "M", marca: "Burberry", category: "pant", price: "300", disc: false },
-            { name: "Boxeri Pull&Bear barbati", sex: "M", marca: "Burberry", category: "boxeri", price: "300", disc: false }];
+            products = [{ name: "Tricou gucci femei", marca: "Gucci", category: "tricou", sex: "F", price: "300", disc: false },
+            { name: "Hanorac Chanel femei", sex: "F", marca: "Chanel", category: "hanorac", price: "300", disc: false },
+            { name: "Geaca Burberry femei", sex: "F", marca: "Burberry", category: "geaca", price: "300", disc: false },
+            { name: "Sosete Zara femei", sex: "F", marca: "Burberry", category: "sosete", price: "300", category: "", disc: false },
+            { name: "Pulover H&M femei", sex: "F", marca: "", category: "pulover", price: "300", disc: false },
+            { name: "Tricou Pull&Bear femei", sex: "F", marca: "Burberry", category: "tricou", price: "300", disc: false },
+            { name: "Camasa gucci femei", sex: "F", marca: "Burberry", category: "camasa", price: "200", disc: true },
+            { name: "Caciula Chanel femei", sex: "F", marca: "Burberry", category: "caciula", price: "300", disc: false },
+            { name: "Manusi Burberry femei", sex: "F", marca: "Burberry", category: "manusi", price: "300", disc: false },
+            { name: "Adiasi Zara femei", sex: "F", marca: "Burberry", category: "incaltari", price: "300", disc: false },
+            { name: "Pantaloni H&M femei", sex: "F", marca: "Burberry", category: "pant", price: "300", disc: false },
+            { name: "Boxeri Pull&Bear femei", sex: "F", marca: "Burberry", category: "boxeri", price: "300", disc: false }];
             break;
 
         case "C":
-            products = [{ name: "Tricou gucci barbati", marca: "Gucci", category: "tricou", sex: "M", price: "300", disc: false },
-            { name: "Hanorac Chanel barbati", sex: "M", marca: "Chanel", category: "hanorac", price: "300", disc: false },
-            { name: "Geaca Burberry barbati", sex: "M", marca: "Burberry", category: "geaca", price: "300", disc: false },
-            { name: "Sosete Zara barbati", sex: "M", marca: "Burberry", category: "sosete", price: "300", category: "", disc: false },
-            { name: "Pulover H&M barbati", sex: "M", marca: "", category: "pulover", price: "300", disc: false },
-            { name: "Tricou Pull&Bear barbati", sex: "M", marca: "Burberry", category: "tricou", price: "300", disc: false },
-            { name: "Camasa gucci barbati", sex: "M", marca: "Burberry", category: "camasa", price: "200", disc: true },
-            { name: "Caciula Chanel barbati", sex: "M", marca: "Burberry", category: "caciula", price: "300", disc: false },
-            { name: "Manusi Burberry barbati", sex: "M", marca: "Burberry", category: "manusi", price: "300", disc: false },
-            { name: "Adiasi Zara barbati", sex: "M", marca: "Burberry", category: "incaltari", price: "300", disc: false },
-            { name: "Pantaloni H&M barbati", sex: "M", marca: "Burberry", category: "pant", price: "300", disc: false },
-            { name: "Boxeri Pull&Bear barbati", sex: "M", marca: "Burberry", category: "boxeri", price: "300", disc: false }];
+            products = [{ name: "Tricou gucci copii", marca: "Gucci", category: "tricou", sex: "C", price: "300", disc: false },
+            { name: "Hanorac Chanel copii", sex: "C", marca: "Chanel", category: "hanorac", price: "300", disc: false },
+            { name: "Geaca Burberry copii", sex: "C", marca: "Burberry", category: "geaca", price: "300", disc: false },
+            { name: "Sosete Zara copii", sex: "C", marca: "Burberry", category: "sosete", price: "300", category: "", disc: false },
+            { name: "Pulover H&M copii", sex: "C", marca: "", category: "pulover", price: "300", disc: false },
+            { name: "Tricou Pull&Bear copii", sex: "C", marca: "Burberry", category: "tricou", price: "300", disc: false },
+            { name: "Camasa gucci copii", sex: "C", marca: "Burberry", category: "camasa", price: "200", disc: true },
+            { name: "Caciula Chanel copii", sex: "C", marca: "Burberry", category: "caciula", price: "300", disc: false },
+            { name: "Manusi Burberry copii", sex: "C", marca: "Burberry", category: "manusi", price: "300", disc: false },
+            { name: "Adiasi Zara copii", sex: "C", marca: "Burberry", category: "incaltari", price: "300", disc: false },
+            { name: "Pantaloni H&M copii", sex: "C", marca: "Burberry", category: "pant", price: "300", disc: false },
+            { name: "Boxeri Pull&Bear copii", sex: "C", marca: "Burberry", category: "boxeri", price: "300", disc: false }];
             break;
         case "R":
             products = [{ name: "Tricou gucci barbati", marca: "Gucci", category: "tricou", sex: "M", price: "300", disc: true },
